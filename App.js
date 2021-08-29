c1a31110b57e93f1c4c2c0da6c41d2469e0820b5
Danny Aziz <daanyaal997@gmail.com>
2021-08-29 19:19:24 +0100

    refactor: clean up App.js

--- App.js
+++ App.js
@@ -7,18 +7,17 @@
  */
 
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, useColorScheme} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import SearchContextProvider, {SearchContext} from '@lib/utils/context';
+import SearchContextProvider from '@lib/utils/context';
 
 import {ThemeProvider} from 'styled-components';
 import {lightTheme, darkTheme} from '@lib/utils/theme';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 import Search from '@lib/views/search';
@@ -28,24 +27,7 @@ import Asset from '@lib/views/asset';
 import {LogBox} from 'react-native';
 LogBox.ignoreLogs(['Reanimated 2']);
 const headers = {
-  // authority: 'api.opensea.io',
-  // pragma: 'no-cache',
-  // 'cache-control': 'no-cache',
-  // 'sec-ch-ua':
-  //   '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-  // accept: '*/*',
-  // 'x-build-id': '6CRSlZgsDn3tDxAEyU_Xx',
-  // 'sec-ch-ua-mobile': '?0',
-  // 'user-agent':
-  //   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
   'x-api-key': '2f6f419a083c46de9d83ce3dbe7db601',
-  // 'content-type': 'application/json',
-  // origin: 'https://opensea.io',
-  // 'sec-fetch-site': 'same-site',
-  // 'sec-fetch-mode': 'cors',
-  // 'sec-fetch-dest': 'empty',
-  // referer: 'https://opensea.io/',
-  // 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
 };
 
 // Initialize Apollo Client
@@ -59,8 +41,7 @@ const client = new ApolloClient({
 const NativeStack = createNativeStackNavigator();
 
 const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark';
-  const isDarkMode = false;
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={`${isDarkMode ? 'light' : 'dark'}-content`} />
@@ -91,6 +72,11 @@ const App = () => {
                   name="Asset"
                   component={Asset}
                   options={{headerShown: false}}
+                  getId={({params}) =>
+                    params.data.asset
+                      ? params.data.asset.relayId
+                      : params.data.assetBundle.relayId
+                  }
                 />
               </NativeStack.Navigator>
             </NavigationContainer>
